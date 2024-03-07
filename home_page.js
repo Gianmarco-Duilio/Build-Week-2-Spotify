569610ba91bb02ae114e4cfd93647fd361dbf0f1
saraferrantini <sara.ferrantini@gmail.com>
2024-03-07 17:10:00 +0100

    modifiche

--- home_page.js
+++ home_page.js
@@ -1,3 +1,4 @@
+// BUONASERA
 const artistNames = ["Eminem", "Nelly", "Ice Cube", "Migos", "Snoop Dogg", "Lil Wayne"];
 function fetchArtistData(artistName) {
   return fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${artistName}`, {
@@ -33,7 +34,7 @@ function createCard(artist) {
       <div id="topCard" class="d-flex rounded-2 bg-secondary ">
           <div><img src="${artist.picture_small}" class="rounded-start" /></div>
           <div class="blc text-white">
-              <p class="truncate-2-lines">${artist.name}</p>
+              <p class="truncate-2-lines my-auto text-white ms-2">${artist.name}</p>
           </div>
       </div>
   `;
@@ -47,3 +48,89 @@ Promise.all(artistNames.map(fetchArtistData)).then((artistsData) => {
     container.appendChild(card);
   });
 });
+
+// -----
+// CARD ALBUM
+// Array degli ID degli album
+const albumIds = [
+  "544893852",
+  "9410086",
+  "12047952",
+  "87722792",
+  "397544257",
+  "384842207",
+
+  "74434962",
+  "108447472",
+  "544674272",
+  "445968695",
+  "544890462",
+  "182811182",
+];
+
+// FETCH
+// Funzione per ottenere il contenuto di un album
+async function fetchAlbum(albumId) {
+  try {
+    const response = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/${albumId}`);
+    if (!response.ok) {
+      throw new Error("Errore durante il recupero dell'album");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+// Funzione per generare le card degli album utilizzando Bootstrap e aggiungere gli event listener
+
+async function generateAlbumCardsWithBootstrapAndListeners() {
+  try {
+    const container = document.getElementById("raccoglitore");
+    let htmlInniettato = "";
+
+    for (let i = 0; i < albumIds.length; i++) {
+      const albumId = albumIds[i];
+      const album = await fetchAlbum(albumId);
+      if (album) {
+        const html = `
+                 
+                  <div class="col-12 col-md-4 col-xl-2">
+                      <div class="card text-white bg-secondary p-2 fixed-size-card" data-album-id="${albumId}">
+                          <img src="${album.cover}" class="card-img-top rounded-2 album" alt="${album.title}">
+                          <div class="card-body">
+                              <h5 class="card-title">${album.title}</h5>
+                              <p class="card-text">${album.artist.name}</p>
+                          </div>
+                      </div>
+                  </div>
+              `;
+        htmlInniettato += html;
+
+        // Aggiungi uno spazio ogni sei card
+        if ((i + 1) % 6 === 0 && i !== albumIds.length - 1) {
+          htmlInniettato += `<div class="spacer"></div>`;
+        }
+      }
+    }
+
+    container.innerHTML = htmlInniettato;
+
+    // Aggiungi gli event listener alle card generate dinamicamente
+    const cards = container.querySelectorAll(".card");
+    cards.forEach((card) => {
+      card.addEventListener("click", function () {
+        const albumId = card.getAttribute("data-album-id");
+        window.location.href = `album_page.html?albumId=${albumId}`;
+      });
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Chiamata alla funzione per generare le card degli album utilizzando Bootstrap e aggiungere gli event listener
+generateAlbumCardsWithBootstrapAndListeners();
+
+// -----
