d25c96b26caf6ea4e896947dde1861ac47c2d2a5
saraferrantini <sara.ferrantini@gmail.com>
2024-03-08 12:00:35 +0100

    modifiche file js

---
+++ artist_page.js
@@ -0,0 +1,33 @@
+// Funzione per ottenere il parametro dell'URL
+function getUrlParameter(name) {
+  name = name.replace(/[[]/, "\\[").replace(/[\]]/, "\\]");
+  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+  var results = regex.exec(location.search);
+  return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+const artistId = getUrlParameter("artistId");
+
+fetch(`https://deezerdevs-deezer.p.rapidapi.com/artist/${artistId}`, {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "9d6bfcc177msh2d11ca23a04202ep104be7jsn9e7420c2619b",
+    "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+  },
+})
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Errore durante il recupero dei dati dell'artista");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log("Dati dell'artista:", data);
+    const artistName = data.name;
+    const artistDetailsContainer = document.getElementById("artistDetails");
+    console.log(artistDetailsContainer);
+    artistDetailsContainer.innerHTML = `<p>Nome Artista: ${artistName}</p>`;
+  })
+  .catch((error) => {
+    console.error("Si è verificato un errore", error);
+  });
--- home_page.js
+++ home_page.js
@@ -1,5 +1,50 @@
+// //☑️LE TUE RIPRODUZIONI - BUONASERA
+// const artistNames = ["Eminem", "Nelly", "Salmo", "Migos", "Mahmood ", "Elodie"];
+// function fetchArtistData(artistName) {
+//   return fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${artistName}`, {
+//     method: "GET",
+//     headers: {
+//       "X-RapidAPI-Key": "9d6bfcc177msh2d11ca23a04202ep104be7jsn9e7420c2619b",
+//       "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+//     },
+//   })
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw new Error("Errore durante il recupero dei dati");
+//       }
+//       return response.json();
+//     })
+//     .then((data) => {
+//       console.log(data);
+//       console.log(data.data[0].artist.picture_small);
+//       return data.data[0].artist;
+//     })
+//     .catch((error) => {
+//       console.error("Si è verificato un errore", error);
+//     });
+// }
+
+// const container = document.querySelector("#cardTop");
+
+// function createCard(artist) {
+//   const card = document.createElement("div");
+//   card.classList.add("col-6", "col-xl-4");
+
+//   card.innerHTML = `
+//       <div id="topCard" class="d-flex rounded-2 bg-secondary ">
+//           <div><img src="${artist.picture_small}" class="rounded-start" /></div>
+//           <div class="blc text-white">
+//               <p class="truncate-2-lines my-auto text-white ms-2">${artist.name}</p>
+//           </div>
+//       </div>
+//   `;
+
+//   return card;
+// }
+
 //☑️LE TUE RIPRODUZIONI - BUONASERA
-const artistNames = ["Eminem", "Nelly", "Ice Cube", "Migos", "Snoop Dogg", "Lil Wayne"];
+const artistNames = ["Eminem", "Nelly", "Salmo", "Migos", "Mahmood ", "Elodie"];
+
 function fetchArtistData(artistName) {
   return fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${artistName}`, {
     method: "GET",
@@ -39,8 +84,24 @@ function createCard(artist) {
       </div>
   `;
 
+  // Aggiungi un gestore dell'evento click alla carta dell'artista
+  card.addEventListener("click", function () {
+    // Modifica l'URL per includere l'ID dell'artista e reindirizza alla pagina dell'artista
+    window.location.href = `artist_page.html?artistId=${artist.id}`; // Assicurati che l'oggetto artista abbia una proprietà "id" che rappresenta l'ID dell'artista
+  });
+
   return card;
 }
+
+window.onload = function () {
+  Promise.all(artistNames.map(fetchArtistData)).then((artistsData) => {
+    console.log(artistsData);
+    artistsData.forEach((artist) => {
+      const card = createCard(artist);
+      container.appendChild(card);
+    });
+  });
+};
 
 // -----
 //☑️CARD ALBUM
@@ -140,7 +201,10 @@ window.onload = function () {
 
 // ☑️HOME PAGE
 
-// 1) ABBIAMO CREATO DINAMICAMENTE LE CARD DELLA HOME PAGE NEL JAVASCRIPT (creando un array di artisti e di id specifici degli album), FATTO UNA FECTCH DI TIPO GET PER PRENDERE I DATI
+// 1) ABBIAMO CREATO DINAMICAMENTE LE CARD DELLA HOME PAGE NEL JAVASCRIPT (creando un array di artisti e di id specifici degli album),
+// e mettendo un ascoltatore sulle card sia album che artisti che all'evento del click mi rimanda alla pagina di album page e artist page.
+
+// FATTO UNA FECTCH DI TIPO GET PER PRENDERE I DATI
 // , CREATO DINAMICAMENTE LE CARD E INNIETTATE NELL'HTML (così che nella home page si visualizzino le card degli artisti scelti)
 
 // ☑️ALBUM PAGE
